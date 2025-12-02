6193ec9f8129cd003ba003515ecec559acfb0504
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-02 18:54:17 +0000

    niceify answer output

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ mod day02;
 
 fn main() {
     let input = fs::read_to_string("input/day02.in").unwrap();
-    println!("{}", day02::part_1(&input));
-    println!("{}", day02::part_2(&input));
+    println!("Part 1: {}", day02::part_1(&input));
+    println!("Part 2: {}", day02::part_2(&input));
 }
