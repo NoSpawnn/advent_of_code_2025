71c6c0c64371a184c3ee0fceaeb5b58aba5d99d3
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-03 17:59:39 +0000

    Switch to rust iterator implementation
    Speedup from 20ms to ~500-600µs!!!!!!!!!!

--- src/03/bin/main.rs
+++ src/03/bin/main.rs
@@ -8,27 +8,35 @@ fn sum_max_jolts(input: &str, max_batteries: usize) -> i64 {
 }
 
 fn find_max_jolt(bank: &str, max_batteries: usize) -> i64 {
-    let mut result_bank = String::from(bank);
+    let mut max_bank = String::from(bank);
 
-    while result_bank.len() > max_batteries {
+    while max_bank.len() > max_batteries {
         let mut removed = false;
-        let mut nums: Vec<_> = result_bank.chars().filter_map(|c| c.to_digit(10)).collect();
-        for i in 0..nums.len() - 1 {
-            if nums[i] < nums[i + 1] {
-                nums.remove(i);
-                result_bank = nums.iter().map(|c| c.to_string()).collect();
+        let mut nums = max_bank
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .enumerate()
+            .peekable();
+
+        while let Some((idx, cur)) = nums.next() {
+            if let Some((_, next)) = nums.peek()
+                && cur < *next
+            {
+                max_bank.remove(idx);
                 removed = true;
                 break;
             }
         }
 
         if !removed {
-            result_bank.truncate(max_batteries);
+            max_bank.truncate(max_batteries);
             break;
         }
     }
 
-    result_bank.parse::<i64>().unwrap()
+    max_bank
+        .parse::<i64>()
+        .unwrap_or_else(|_| panic!("Input is invalid, got: {}", max_bank))
 }
 
 pub fn part_1(input: &str) -> i64 {
@@ -40,8 +48,8 @@ pub fn part_2(input: &str) -> i64 {
 }
 
 fn main() {
+    // let input = std::fs::read_to_string("input/03.example").unwrap();
     let input = std::fs::read_to_string("input/03.in").unwrap();
-    // let input = std::fs::read_to_string("input/03.in").unwrap();
     println!("Part 1: {}", part_1(&input));
     println!("Part 2: {}", part_2(&input));
 }
