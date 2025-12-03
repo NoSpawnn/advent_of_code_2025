386e302cfa0c63dc86c486c3bd1af45e1b24a061
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-03 17:29:49 +0000

    day 3 part 2

--- src/03/bin/main.rs
+++ src/03/bin/main.rs
@@ -1,58 +1,47 @@
 // https://adventofcode.com/2025/day/3
 
-// each line of digits in your input corresponds to a single bank of batteries.
-// Within each bank, you need to turn on exactly two batteries
-
-pub fn part_1(input: &str) -> u32 {
-    let mut sum = 0;
-
-    for line in input.lines() {
-        let mut m1: Option<u32> = None;
-        let mut m2: Option<u32> = None;
-
-        let chars: Vec<_> = line.chars().collect();
-        let mut ptr = 0usize;
-        for i in 0..chars.len() {
-            let n = chars[i].to_digit(10).unwrap();
-            if Some(n) > m1 {
-                m1 = Some(n);
-                ptr = i;
-            }
-        }
-
-        // If the largest number is at the end of the string,
-        // we wont use it as the x10
-        if ptr == chars.len() - 1 {
-            m2 = m1;
-            m1 = None;
-            for i in 0..chars.len() - 1 {
-                let n = chars[i].to_digit(10).unwrap();
-                if Some(n) > m1 {
-                    m1 = Some(n);
-                }
-            }
-        } else {
-            for i in ptr + 1..chars.len() {
-                let n = chars[i].to_digit(10).unwrap();
-                if Some(n) > m2 {
-                    m2 = Some(n);
-                }
-            }
-        }
-
-        sum += m1.unwrap() * 10 + m2.unwrap()
-    }
-
-    sum
+fn sum_max_jolts(input: &str, max_batteries: usize) -> i64 {
+    input
+        .lines()
+        .map(|line| find_max_jolt(line, max_batteries))
+        .sum()
 }
 
-pub fn part_2(input: &str) -> i32 {
-    0
+fn find_max_jolt(bank: &str, max_batteries: usize) -> i64 {
+    let mut result_bank = String::from(bank);
+
+    while result_bank.len() > max_batteries {
+        let mut removed = false;
+        let mut nums: Vec<_> = result_bank.chars().filter_map(|c| c.to_digit(10)).collect();
+        for i in 0..nums.len() - 1 {
+            if nums[i] < nums[i + 1] {
+                nums.remove(i);
+                result_bank = nums.iter().map(|c| c.to_string()).collect();
+                removed = true;
+                break;
+            }
+        }
+
+        if !removed {
+            result_bank.truncate(max_batteries);
+            break;
+        }
+    }
+
+    result_bank.parse::<i64>().unwrap()
+}
+
+pub fn part_1(input: &str) -> i64 {
+    sum_max_jolts(input, 2)
+}
+
+pub fn part_2(input: &str) -> i64 {
+    sum_max_jolts(input, 12)
 }
 
 fn main() {
-    // let input = std::fs::read_to_string("input/03.example").unwrap();
     let input = std::fs::read_to_string("input/03.in").unwrap();
+    // let input = std::fs::read_to_string("input/03.in").unwrap();
     println!("Part 1: {}", part_1(&input));
     println!("Part 2: {}", part_2(&input));
 }
