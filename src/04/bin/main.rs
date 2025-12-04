f4a338db5f712b0373b75a182ef0cfda01ea79d1
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-04 19:49:04 +0000

    Flag-based hashmap

--- src/04/bin/main.rs
+++ src/04/bin/main.rs
@@ -1,12 +1,15 @@
-use std::collections::HashSet;
-
 #[rustfmt::skip]
 const OFFSETS: [(isize, isize); 8] = [
     (-1, -1), (-1, 0), (-1, 1),
     ( 0, -1),          ( 0, 1),
     ( 1, -1), ( 1, 0), ( 1, 1)
 ];
-type PaperRollCoords = std::collections::HashSet<(usize, usize)>;
+type PaperRollCoords = std::collections::HashMap<(usize, usize), PaperRollState>;
+
+enum PaperRollState {
+    Present,
+    Absent,
+}
 
 fn count_adjacent(grid: &PaperRollCoords, row: usize, col: usize) -> i32 {
     OFFSETS
@@ -15,7 +18,7 @@ fn count_adjacent(grid: &PaperRollCoords, row: usize, col: usize) -> i32 {
             let row_to_check = (row as isize) + row_offset;
             let col_to_check = (col as isize) + col_offset;
             let coord = (row_to_check as usize, col_to_check as usize);
-            grid.contains(&coord)
+            matches!(grid.get(&coord), Some(PaperRollState::Present))
         })
         .count() as i32
 }
@@ -25,8 +28,9 @@ fn make_grid(lines: &str) -> PaperRollCoords {
         .lines()
         .enumerate()
         .flat_map(|(row_idx, row)| {
-            row.char_indices()
-                .filter_map(move |(col_idx, chr)| (chr == '@').then(|| (row_idx, col_idx)))
+            row.char_indices().filter_map(move |(col_idx, chr)| {
+                (chr == '@').then(|| ((row_idx, col_idx), PaperRollState::Present))
+            })
         })
         .collect()
 }
@@ -34,34 +38,35 @@ fn make_grid(lines: &str) -> PaperRollCoords {
 pub fn part_1(input: &str) -> i32 {
     let grid = make_grid(input);
     grid.iter()
-        .filter(|(row, col)| count_adjacent(&grid, *row, *col) < 4)
+        .filter(|((row, col), _)| count_adjacent(&grid, *row, *col) < 4)
         .count() as i32
 }
 
 pub fn part_2(input: &str) -> i32 {
     let mut grid = make_grid(input);
-    let start_len = grid.len();
+    let start_len = grid
+        .iter()
+        .filter(|(_, s)| matches!(s, PaperRollState::Present))
+        .count();
 
     loop {
-        let old_len = grid.len();
-        let to_remove: Vec<_> = grid
+        let new_grid: PaperRollCoords = grid
             .iter()
-            .filter_map(|(row, col)| {
-                if count_adjacent(&grid, *row, *col) < 4 {
-                    Some((*row, *col))
-                } else {
-                    None
-                }
+            .filter_map(|((row, col), _)| {
+                (count_adjacent(&grid, *row, *col) >= 4)
+                    .then_some(((*row, *col), PaperRollState::Present))
             })
             .collect();
+        let old_len = grid
+            .iter()
+            .filter(|(_, s)| matches!(s, PaperRollState::Present))
+            .count();
 
-        for coord in to_remove {
-            grid.remove(&coord);
-        }
-
-        if old_len == grid.len() {
+        if old_len == new_grid.len() {
             return (start_len - old_len) as i32;
         }
+
+        grid = new_grid;
     }
 }
 
