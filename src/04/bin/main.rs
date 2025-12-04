aa61cb050354c3e559ae00119f551707bfa5d2eb
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-04 22:25:13 +0000

    1D grid implementation

--- src/04/bin/main.rs
+++ src/04/bin/main.rs
@@ -1,8 +1,8 @@
 // https://adventofcode.com/2025/day/4
 
 // https://www.reddit.com/r/csharp/comments/1b5wglp/how_to_convert_a_2d_index_into_a_1d_index/
-// array2D[A_LENGTH][B_LENGTH] -> array1D: index = (b_index * A_LENGTH) + a_index
 
+#[derive(Debug)]
 struct Grid1D<T> {
     values: Vec<T>,
     width: usize,
@@ -36,33 +36,43 @@ impl<T> Grid1D<T> {
         ( 1, -1), ( 1, 0), ( 1, 1)
     ];
 
-    fn at(&self, row: usize, col: usize) -> Option<&T> {
-        if let Some(idx) = self.idx(row, col)
-            && let Some(value) = self.values.get(idx)
-        {
-            Some(value)
+    fn get_from_1d_index(&self, idx: usize) -> Option<&T> {
+        self.values.get(idx)
+    }
+
+    fn get_from_2d_index(&self, row: usize, col: usize) -> Option<&T> {
+        if let Some(idx) = self.get_1d_index(row, col) {
+            self.values.get(idx)
         } else {
             None
         }
     }
 
-    fn idx(&self, row: usize, col: usize) -> Option<usize> {
-        let idx = (col * self.height) + row;
+    fn get_1d_index(&self, row: usize, col: usize) -> Option<usize> {
+        let idx = (row * self.height) + col;
         if idx < self.values.len() {
             Some(idx)
         } else {
             None
         }
     }
+
+    fn count_by<F: Fn(&T) -> bool>(&self, f: F) -> usize {
+        self.values.iter().filter(|v| f(*v)).count()
+    }
+
+    fn len(&self) -> usize {
+        self.values.len()
+    }
 }
 
 impl Grid1D<PaperRollState> {
-    fn count_adjacent(&self, row: usize, col: usize) -> i32 {
-        Self::OFFSETS
+    fn count_adjacent_2d(&self, row: usize, col: usize) -> i32 {
+        let res = Self::OFFSETS
             .iter()
             .filter(|(row_offset, col_offset)| {
-                let row_to_check = (row as isize) + row_offset;
-                let col_to_check = (col as isize) + col_offset;
+                let row_to_check = row as isize + row_offset;
+                let col_to_check = col as isize + col_offset;
 
                 if row_to_check < 0
                     || col_to_check < 0
@@ -72,13 +82,20 @@ impl Grid1D<PaperRollState> {
                     return false;
                 }
 
-                if let Some(idx) = self.idx(row_to_check as usize, col_to_check as usize) {
-                    matches!(self.values[idx as usize], PaperRollState::Present)
-                } else {
-                    false
-                }
+                matches!(
+                    self.get_from_2d_index(row_to_check as usize, col_to_check as usize),
+                    Some(PaperRollState::Present)
+                )
             })
-            .count() as i32
+            .count() as i32;
+
+        res
+    }
+
+    fn count_adjacent_1d(&self, idx: usize) -> i32 {
+        let row = idx / self.width;
+        let col = idx % self.width;
+        self.count_adjacent_2d(row, col)
     }
 }
 
@@ -99,50 +116,48 @@ impl From<char> for PaperRollState {
 
 pub fn part_1(input: &str) -> i32 {
     let grid: Grid1D<PaperRollState> = Grid1D::from(input);
-    let mut res = 0;
-    for row in 0..input.lines().count() {
-        for col in 0..input.lines().next().unwrap().len() {
-            if !matches!(grid.at(row, col), Some(PaperRollState::Absent))
-                && grid.count_adjacent(row, col) < 4
-            {
-                res += 1;
-            }
-        }
-    }
-    res
+    grid.values
+        .iter()
+        .enumerate()
+        .filter(|(idx, _)| {
+            matches!(grid.get_from_1d_index(*idx), Some(PaperRollState::Present))
+                && grid.count_adjacent_1d(*idx) < 4
+        })
+        .count() as i32
 }
 
-// pub fn part_2(input: &str) -> i32 {
-//     let mut grid = make_grid(input);
-//     let start_len = grid
-//         .iter()
-//         .filter(|(_, s)| matches!(s, PaperRollState::Present))
-//         .count();
+pub fn part_2(input: &str) -> i32 {
+    let mut grid: Grid1D<PaperRollState> = Grid1D::from(input);
+    let start_count = grid.count_by(|v| matches!(v, PaperRollState::Present));
 
-//     loop {
-//         let new_grid: PaperRollCoords = grid
-//             .iter()
-//             .filter_map(|((row, col), _)| {
-//                 (count_adjacent(&grid, *row, *col) >= 4)
-//                     .then_some(((*row, *col), PaperRollState::Present))
-//             })
-//             .collect();
-//         let old_len = grid
-//             .iter()
-//             .filter(|(_, s)| matches!(s, PaperRollState::Present))
-//             .count();
-
-//         if old_len == new_grid.len() {
-//             return (start_len - old_len) as i32;
-//         }
-
-//         grid = new_grid;
-//     }
-// }
+    loop {
+        let new_values: Vec<_> = grid
+            .values
+            .iter()
+            .enumerate()
+            .map(|(idx, _)| {
+                if matches!(grid.get_from_1d_index(idx), Some(PaperRollState::Present)) {
+                    match grid.count_adjacent_1d(idx) {
+                        0..4 => PaperRollState::Absent,
+                        _ => PaperRollState::Present,
+                    }
+                } else {
+                    PaperRollState::Absent
+                }
+            })
+            .collect();
+        let old_count = grid.count_by(|v| matches!(v, PaperRollState::Present));
+        grid.values = new_values;
+        let new_count = grid.count_by(|v| matches!(v, PaperRollState::Present));
+        if old_count == new_count {
+            return (start_count - old_count) as i32;
+        }
+    }
+}
 
 fn main() {
     // let input = std::fs::read_to_string("input/04.example").unwrap();
     let input = std::fs::read_to_string("input/04.in").unwrap();
     println!("Part 1: {}", part_1(&input));
-    // println!("Part 2: {}", part_2(&input));
+    println!("Part 2: {}", part_2(&input));
 }
