c0e672af3f824b5a2c14e9acb9ec1f26aab3fddb
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-04 19:27:01 +0000

    Simplify

--- src/04/bin/main.rs
+++ src/04/bin/main.rs
@@ -1,6 +1,5 @@
 use std::collections::HashSet;
 
-const ROLL: char = '@';
 #[rustfmt::skip]
 const OFFSETS: [(isize, isize); 8] = [
     (-1, -1), (-1, 0), (-1, 1),
@@ -9,18 +8,14 @@ const OFFSETS: [(isize, isize); 8] = [
 ];
 type PaperRollCoords = HashSet<(usize, usize)>;
 
-fn count_adjacent(grid: &HashSet<(usize, usize)>, row: usize, col: usize) -> i32 {
+fn count_adjacent(grid: &PaperRollCoords, row: usize, col: usize) -> i32 {
     OFFSETS
         .iter()
         .filter(|(row_offset, col_offset)| {
-            if let Some(row_to_check) = (row as isize).checked_add(*row_offset)
-                && let Some(col_to_check) = (col as isize).checked_add(*col_offset)
-            {
-                let coord = (row_to_check as usize, col_to_check as usize);
-                grid.contains(&coord)
-            } else {
-                false
-            }
+            let row_to_check = (row as isize) + row_offset;
+            let col_to_check = (col as isize) + col_offset;
+            let coord = (row_to_check as usize, col_to_check as usize);
+            grid.contains(&coord)
         })
         .count() as i32
 }
@@ -31,7 +26,7 @@ fn make_grid(lines: &str) -> PaperRollCoords {
         .enumerate()
         .map(|(row_idx, row)| {
             row.char_indices().filter_map(move |(col_idx, chr)| {
-                if chr == ROLL {
+                if chr == '@' {
                     Some((row_idx, col_idx))
                 } else {
                     None
@@ -44,7 +39,6 @@ fn make_grid(lines: &str) -> PaperRollCoords {
 
 pub fn part_1(input: &str) -> i32 {
     let grid = make_grid(input);
-
     grid.iter()
         .filter(|(row, col)| count_adjacent(&grid, *row, *col) < 4)
         .count() as i32
