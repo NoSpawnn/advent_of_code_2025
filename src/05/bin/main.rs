2182e75a1678bb08535f5d4f03ab725c4259d24c
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-05 12:58:32 +0000

    Day 5 part 2

--- src/05/bin/main.rs
+++ src/05/bin/main.rs
@@ -1,8 +1,9 @@
 // https://adventofcode.com/2025/day/5
 
-pub fn part_1(input: &str) -> i64 {
-    let (ranges, ids) = input.split_once("\n\n").unwrap();
-    let ranges: Vec<std::ops::RangeInclusive<i64>> = ranges
+use std::cmp::{max, min};
+
+fn parse_ranges(lines: &str) -> Vec<std::ops::RangeInclusive<i64>> {
+    let mut ranges: Vec<std::ops::RangeInclusive<i64>> = lines
         .lines()
         .map(|r| {
             let (start, end) = r.split_once('-').unwrap();
@@ -10,7 +11,25 @@ pub fn part_1(input: &str) -> i64 {
             start..=end
         })
         .collect();
+    ranges.sort_by_key(|r| *r.start());
+
+    while let Some((existing_idx, [w0, w1])) = ranges
+        .windows(2)
+        .enumerate()
+        .find(|(_, w)| w[0].start() <= w[1].end() && w[1].start() <= w[0].end())
+    {
+        ranges[existing_idx] = min(*w0.start(), *w1.start())..=max(*w0.end(), *w1.end());
+        ranges.remove(existing_idx + 1);
+    }
+
+    ranges
+}
+
+pub fn part_1(input: &str) -> i64 {
+    let (ranges, ids) = input.split_once("\n\n").unwrap();
+    let ranges = parse_ranges(ranges);
     let mut fresh = 0;
+    dbg!(&ranges);
 
     for id in ids.lines() {
         let id = id.parse::<i64>().unwrap();
@@ -22,8 +41,16 @@ pub fn part_1(input: &str) -> i64 {
     fresh
 }
 
-pub fn part_2(input: &str) -> i32 {
-    0
+pub fn part_2(input: &str) -> i64 {
+    let (ranges, _) = input.split_once("\n\n").unwrap();
+    let ranges = parse_ranges(ranges);
+
+    let mut fresh = 0;
+    for r in ranges {
+        fresh += r.count() as i64
+    }
+
+    fresh
 }
 
 fn main() {
