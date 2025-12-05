82a67211573bbf1ecfc76e6096d57e1003fad32d
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-05 11:16:33 +0000

    Day 5 part 1

---
+++ src/05/bin/main.rs
@@ -0,0 +1,36 @@
+// https://adventofcode.com/2025/day/5
+
+use std::collections::HashSet;
+
+pub fn part_1(input: &str) -> i64 {
+    let (ranges, ids) = input.split_once("\n\n").unwrap();
+    let ranges: HashSet<std::ops::RangeInclusive<i64>> = ranges
+        .lines()
+        .map(|r| {
+            let (start, end) = r.split_once('-').unwrap();
+            let (start, end): (i64, i64) = (start.parse().unwrap(), end.parse().unwrap());
+            start..=end
+        })
+        .collect();
+    let mut fresh = 0;
+
+    for id in ids.lines() {
+        let id = id.parse::<i64>().unwrap();
+        if ranges.iter().any(|r| r.contains(&id)) {
+            fresh += 1
+        }
+    }
+
+    fresh
+}
+
+pub fn part_2(input: &str) -> i32 {
+    0
+}
+
+fn main() {
+    // let input = std::fs::read_to_string("input/05.example").unwrap();
+    let input = std::fs::read_to_string("input/05.in").unwrap();
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}
