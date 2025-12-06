e4835de9e5c654e39f021d36b953758ee635acf3
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-06 21:32:20 +0000

    move parse functions into relevant struct

--- src/06/main.rs
+++ src/06/main.rs
@@ -8,6 +8,54 @@ struct Problem {
     nums: Vec<i64>,
 }
 
+impl Problem {
+    fn parse_one(lines: &[&str], col_from: usize, col_to: usize, column_wise: bool) -> Self {
+        let block: Vec<_> = lines
+            .iter()
+            .take(lines.len() - 1) // we care not about the last line for numbers parsing
+            .map(move |line| &line[col_from..col_to])
+            .collect();
+
+        let op = lines.last().expect("input empty")[col_from..col_to]
+            .chars()
+            .next()
+            .expect("operator line empty");
+
+        if column_wise {
+            let nums = transpose(&block)
+                .map(|col| col.trim().parse::<i64>().unwrap())
+                .collect();
+
+            return Problem { op, nums };
+        }
+
+        let nums = block
+            .iter()
+            .map(|col| col.trim().parse::<i64>().unwrap())
+            .collect();
+
+        Self { op, nums }
+    }
+
+    fn parse_many<'a>(lines: &'a str, column_wise: bool) -> Vec<Self> {
+        let lines: Vec<_> = lines.lines().collect();
+        let col_end = lines[0].len();
+        let mut block_start = 0;
+        let mut problems = Vec::with_capacity(1000); // there's 1000 problems on the real input, this is a 10-15µs time save
+
+        for col in 0..col_end {
+            if get_column(&lines, &col).all(|c| c.is_whitespace()) {
+                let problem = Self::parse_one(&lines, block_start, col, column_wise);
+                problems.push(problem);
+                block_start = col + 1;
+            }
+        }
+
+        problems.push(Self::parse_one(&lines, block_start, col_end, column_wise));
+        problems
+    }
+}
+
 fn get_column<'a>(lines: &'a [&str], col: &'a usize) -> impl Iterator<Item = char> {
     lines.iter().map(|line| char::from(line.as_bytes()[*col]))
 }
@@ -17,54 +65,8 @@ fn transpose<'a>(lines: &'a [&str]) -> impl Iterator<Item = String> {
     (0..len).map(|col| get_column(lines, &col).collect())
 }
 
-fn parse_problem(lines: &[&str], col_from: usize, col_to: usize, column_wise: bool) -> Problem {
-    let block: Vec<_> = lines
-        .iter()
-        .take(lines.len() - 1) // we care not about the last line for numbers parsing
-        .map(move |line| &line[col_from..col_to])
-        .collect();
-
-    let op = lines.last().expect("input empty")[col_from..col_to]
-        .chars()
-        .next()
-        .expect("operator line empty");
-
-    if column_wise {
-        let nums = transpose(&block)
-            .map(|col| col.trim().parse::<i64>().unwrap())
-            .collect();
-
-        return Problem { op, nums };
-    }
-
-    let nums = block
-        .iter()
-        .map(|col| col.trim().parse::<i64>().unwrap())
-        .collect();
-
-    Problem { op, nums }
-}
-
-fn parse_problems<'a>(lines: &'a str, column_wise: bool) -> Vec<Problem> {
-    let lines: Vec<_> = lines.lines().collect();
-    let col_end = lines[0].len();
-    let mut block_start = 0;
-    let mut problems = Vec::with_capacity(1000); // There's 1000 problems on the real input, this is a 10-15µs time save
-
-    for col in 0..col_end {
-        if get_column(&lines, &col).all(|c| c.is_whitespace()) {
-            let problem = parse_problem(&lines, block_start, col, column_wise);
-            problems.push(problem);
-            block_start = col + 1;
-        }
-    }
-
-    problems.push(parse_problem(&lines, block_start, col_end, column_wise));
-    problems
-}
-
 pub fn part_1(input: &str) -> Answer {
-    parse_problems(input, false)
+    Problem::parse_many(input, false)
         .iter()
         .fold(0, |acc, problem| match problem.op {
             '*' => acc + problem.nums.iter().fold(1, |acc, n| acc * n),
@@ -74,7 +76,7 @@ pub fn part_1(input: &str) -> Answer {
 }
 
 pub fn part_2(input: &str) -> Answer {
-    parse_problems(input, true)
+    Problem::parse_many(input, true)
         .iter()
         .fold(0, |acc, problem| match problem.op {
             '*' => acc + problem.nums.iter().fold(1, |acc, n| acc * n),
