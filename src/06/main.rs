914f0c3107895741407c6817b3add5e4b0235d6e
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-06 20:44:30 +0000

    Refactor day 6 slightly

--- src/06/main.rs
+++ src/06/main.rs
@@ -2,6 +2,7 @@
 
 type Answer = i64;
 
+#[derive(Debug)]
 struct Problem {
     op: char,
     nums: Vec<i64>,
@@ -16,16 +17,34 @@ fn transpose(lines: &[String]) -> impl Iterator<Item = String> {
     (0..len).map(|col| get_column(lines, &col).collect())
 }
 
-fn parse_problem(lines: &[String], col_from: usize, col_to: usize) -> impl Iterator<Item = String> {
-    lines.iter().map(move |line| {
-        line.chars()
-            .skip(col_from)
-            .take(col_to - col_from)
-            .collect()
-    })
+fn parse_problem(lines: &[String], col_from: usize, col_to: usize, column_wise: bool) -> Problem {
+    let mut block: Vec<_> = lines
+        .iter()
+        .map(move |line| {
+            line.chars()
+                .skip(col_from)
+                .take(col_to - col_from)
+                .collect::<String>()
+        })
+        .collect();
+
+    let op = block.last().unwrap().chars().nth(0).unwrap();
+
+    if column_wise {
+        block = transpose(&block[..block.len() - 1]).collect();
+    } else {
+        block.drain(block.len() - 1..);
+    }
+
+    let nums = block
+        .iter()
+        .map(|col| col.trim().parse::<i64>().unwrap())
+        .collect();
+
+    Problem { op, nums }
 }
 
-fn parse_problems<'a>(lines: &'a str) -> Vec<Vec<String>> {
+fn parse_problems<'a>(lines: &'a str, column_wise: bool) -> Vec<Problem> {
     let lines: Vec<_> = lines.lines().map(str::to_owned).collect();
     let col_end = lines[0].len();
     let mut block_start = 0;
@@ -34,51 +53,33 @@ fn parse_problems<'a>(lines: &'a str) -> Vec<Vec<String>> {
     for col in 0..col_end {
         let column = get_column(&lines, &col).collect::<String>();
         if column.trim().is_empty() {
-            let problem_lines = parse_problem(&lines, block_start, col);
-            problems.push(problem_lines.collect());
+            let problem = parse_problem(&lines, block_start, col, column_wise);
+            problems.push(problem);
             block_start = col + 1;
         }
     }
 
-    problems.push(parse_problem(&lines, block_start, col_end).collect());
+    problems.push(parse_problem(&lines, block_start, col_end, column_wise));
     problems
 }
 
 pub fn part_1(input: &str) -> Answer {
-    parse_problems(input).iter().fold(0, |acc, problem| {
-        let op = problem.last().unwrap().chars().nth(0).unwrap();
-        let nums = problem[..problem.len() - 1]
-            .iter()
-            .map(|col| col.trim().parse::<i64>().unwrap());
-        match op {
-            '*' => acc + nums.fold(1, |acc, n| acc * n),
-            '+' => acc + nums.sum::<i64>(),
-            _ => panic!("unknown operator {}", op),
-        }
-    })
+    parse_problems(input, false)
+        .iter()
+        .fold(0, |acc, problem| match problem.op {
+            '*' => acc + problem.nums.iter().fold(1, |acc, n| acc * n),
+            '+' => acc + problem.nums.iter().sum::<i64>(),
+            _ => panic!("unknown operator {}", problem.op),
+        })
 }
 
 pub fn part_2(input: &str) -> Answer {
-    let problems = parse_problems(input);
-    problems
-        .iter()
-        .map(|p| transpose(&p).peekable())
-        .fold(0, |acc, mut problem| {
-            let op = problem.peek().unwrap().chars().last().unwrap();
-            let nums = problem.map(|col| {
-                col.chars()
-                    .take(col.len() - 1)
-                    .collect::<String>()
-                    .trim()
-                    .parse::<i64>()
-                    .unwrap()
-            });
-            match op {
-                '*' => acc + nums.fold(1, |acc, n| acc * n),
-                '+' => acc + nums.sum::<i64>(),
-                _ => panic!("unknown operator {}", op),
-            }
-        })
+    let problems = parse_problems(input, true);
+    problems.iter().fold(0, |acc, problem| match problem.op {
+        '*' => acc + problem.nums.iter().fold(1, |acc, n| acc * n),
+        '+' => acc + problem.nums.iter().sum::<i64>(),
+        _ => panic!("unknown operator {}", problem.op),
+    })
 }
 
 fn main() {
