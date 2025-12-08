fab6bd229858629118a398861fc8a2f5f64fb3f7
Red <78027161+NoSpawnn@users.noreply.github.com>
2025-12-08 19:06:15 +0000

    cleanup d8

--- src/08/main.rs
+++ src/08/main.rs
@@ -24,6 +24,7 @@ impl From<&str> for JunctionBox {
 
 impl JunctionBox {
     // distance = sqrt( (x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2 )
+    // but we store the square distance to save sqrt() computation
     // https://math.stackexchange.com/questions/42640/calculate-distance-in-3d-space
     //   namely https://math.stackexchange.com/a/1069627
     fn euclid_distance_to(&self, other: &JunctionBox) -> f32 {
@@ -51,7 +52,7 @@ impl JunctionBox {
     }
 }
 
-fn find_circuits<'a>(
+fn connect<'a>(
     from: &'a JunctionBox,
     to: &'a JunctionBox,
     circuits: &mut Vec<Vec<&'a JunctionBox>>,
@@ -95,56 +96,55 @@ fn find_circuits<'a>(
     }
 }
 
-pub fn part_1(input: &str) -> Answer {
-    const N_CONNECTIONS: usize = 1000;
-    let junction_boxes: Vec<_> = input.lines().map(JunctionBox::from).collect();
-    let mut possible_connections: Vec<_> = junction_boxes
+/// Returns all possible connections sorted ascending by length
+fn connection_combinations_by_len<'a>(
+    points: &'a [JunctionBox],
+) -> Vec<(&'a JunctionBox, &'a JunctionBox)> {
+    let mut possible_connections: Vec<_> = points
         .iter()
         .enumerate()
-        .flat_map(|(i, j1)| junction_boxes[i + 1..].iter().map(move |j2| (j1, j2)))
+        .flat_map(|(i, j1)| points[i + 1..].iter().map(move |j2| (j1, j2)))
         .collect();
     possible_connections.sort_by(|j1, j2| {
         let d1 = j1.0.euclid_distance_to(j1.1);
         let d2 = j2.0.euclid_distance_to(j2.1);
         d1.total_cmp(&d2)
     });
+    possible_connections
+}
+
+pub fn part_1(input: &str) -> Answer {
+    const N_CONNECTIONS: usize = 1000;
+    let junction_boxes: Vec<_> = input.lines().map(JunctionBox::from).collect();
+    let possible_connections = connection_combinations_by_len(&junction_boxes);
     let mut circuits: Vec<Vec<&JunctionBox>> = Vec::new();
 
-    for (from, to) in possible_connections.iter().take(N_CONNECTIONS) {
-        find_circuits(from, to, &mut circuits);
-    }
+    possible_connections
+        .iter()
+        .take(N_CONNECTIONS)
+        .for_each(|(from, to)| connect(from, to, &mut circuits));
 
     // sort descending by length
     circuits.sort_by(|a, b| b.len().cmp(&a.len()));
-    circuits.iter().take(3).map(|it| it.len()).product()
+    circuits.into_iter().take(3).map(|it| it.len()).product()
 }
 
 pub fn part_2(input: &str) -> Answer {
     let junction_boxes: Vec<_> = input.lines().map(JunctionBox::from).collect();
-    let mut possible_connections: Vec<_> = junction_boxes
-        .iter()
-        .enumerate()
-        .flat_map(|(i, j1)| junction_boxes[i + 1..].iter().map(move |j2| (j1, j2)))
-        .collect();
-    possible_connections.sort_by(|j1, j2| {
-        let d1 = j1.0.euclid_distance_to(j1.1);
-        let d2 = j2.0.euclid_distance_to(j2.1);
-        d1.total_cmp(&d2)
+    let target_len = junction_boxes.len();
+    let possible_connections = connection_combinations_by_len(&junction_boxes);
+    let mut circuits: Vec<Vec<&JunctionBox>> = Vec::new();
+
+    let last = possible_connections.into_iter().cycle().find(|(from, to)| {
+        connect(from, to, &mut circuits);
+        circuits[0].len() == target_len
     });
 
-    let mut circuits: Vec<Vec<&JunctionBox>> = Vec::new();
-    let mut last: Option<(&JunctionBox, &JunctionBox)> = None;
-
-    for (from, to) in possible_connections.iter().cycle() {
-        find_circuits(from, to, &mut circuits);
-        if circuits[0].len() == junction_boxes.len() {
-            last = Some((from, to));
-            break;
-        }
+    if let Some(last) = last {
+        last.0.x * last.1.x
+    } else {
+        unreachable!()
     }
-
-    let last = last.unwrap();
-    last.0.x * last.1.x
 }
 
 fn main() {
